aad4372a8570a1c67b54e19163ab648192ee46a8
Justin Pincar <justinpincar@gmail.com>
2011-09-03 20:21:51 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inksling::Application.config.secret_token = '608966be648f8d6540b113148a779f6fab390ade2ecc0213d0ccf0a0178538bf6ecb6c72c5b917dcecc4a5df0c5d7504086216ac6752aac2b6c8e671929e5b56'
