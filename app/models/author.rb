aba2fac6d4cdea30eaefa06c5ffecf7f54b65b15
Justin Pincar <justinpincar@gmail.com>
2011-09-04 16:21:38 -0700

    Gravatar.

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,13 @@ class Author < ActiveRecord::Base
   attr_accessible :email, :password, :remember_me, :autobiography
 
   has_many :inks
+
+  def self.thumbnail_url
+    "http://www.gravatar.com/avatar/00000000000000000000000000000000?s=512&d=mm"
+  end
+
+  def thumbnail_url
+    hash = Digest::MD5.hexdigest(self.email)
+    "http://www.gravatar.com/avatar/#{hash}?s=512&d=mm"
+  end
 end
