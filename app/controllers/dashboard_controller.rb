b9b4750787abacc93f66e6edfcad02cc0cd85c91
Justin Pincar <justinpincar@gmail.com>
2011-09-05 22:29:13 -0700

    Filling out dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_filter :authenticate_author!
 
   def index
-    # This method intentionally left blank.
+    @inks = Ink.where("text IS NOT NULL AND author_id != ?", current_author.id).order("updated_at ASC").limit(5)
   end
 end
